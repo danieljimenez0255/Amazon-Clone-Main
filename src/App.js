6976180f2c8a40b38a24802787248a77f286d4b4
danieljimenez0255 <42664403+danieljimenez0255@users.noreply.github.com>
2020-09-17 09:55:17 -0700

    updated main js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
+import Spinner from "react-bootstrap/Spinner";
 
 function App() {
+  const [aniState, setAniState] = useState(false);
+  const [spinState, setSpinState] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAniState(true);
+    }, 1500);
+    setTimeout(() => {
+      setSpinState(true);
+    }, 1000);
+  }, []);
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           {/* Make sure default route is always at bottom.  */}
           <Route path="/">
-            <Home />
+            {spinState === true ? (
+              <>
+                <Header cAniState={aniState} />
+                <Home cAniState={aniState} />
+              </>
+            ) : (
+              <Spinner animation="border" variant="info">
+                <h1 style={{ textAlign: "center" }}>
+                  Amazon clone is loading...
+                </h1>
+              </Spinner>
+            )}
           </Route>
         </Switch>
       </div>
